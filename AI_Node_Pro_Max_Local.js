bbc76231c6d2b55d3cf9e0aaad2ba624b6d64e13
Nishuoxihuan <roelmba@gmail.com>
2026-07-14 23:15:27 -0700

    Update AI_Node_Pro_Max_Local.js

--- AI_Node_Pro_Max_Local.js
+++ AI_Node_Pro_Max_Local.js
@@ -2,7 +2,7 @@ $httpClient.get("https://api.my-ip.io/ip", function(error, response, data) {
   if (error) {
     $done({
       title: "节点体检 Pro Max",
-      content: "外网请求失败\\nerror: " + JSON.stringify(error),
+      content: "外网请求失败\n" + JSON.stringify(error),
       style: "alert"
     });
     return;
@@ -10,7 +10,7 @@ $httpClient.get("https://api.my-ip.io/ip", function(error, response, data) {
 
   $done({
     title: "节点体检 Pro Max",
-    content: "外网请求成功\\nIP: " + data,
+    content: "外网请求成功\nIP: " + String(data).trim(),
     style: "good"
   });
 });
